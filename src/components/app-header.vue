1766e807503fcfebc1be627ffc3ac86a5dd2138a
浮尘 <757213882@qq.com>
2017-09-28 18:03:02 +0800

    修改首页，添加header组件和menu组件

---
+++ src/components/app-header.vue
@@ -0,0 +1,83 @@
+<template>
+	<nav class="nav">
+		<div class="container">
+			<div class="hamburger-menu lt">
+				<div class="hamburger-item hamburger-top"></div>
+				<div class="hamburger-item hamburger-middle"></div>
+				<div class="hamburger-item hamburger-bottom"></div>
+			</div>
+			<div class="header-search rt"></div>
+			<h3 class="site-name">
+				<router-link to="/">
+					<img class="site-logo" src="../assets/site-logo.png">
+				</router-link>
+			</h3>
+		</div>
+	</nav>
+</template>
+
+<script>
+export default {
+	name: "appHeader"
+}
+</script>
+
+<style lang="less">
+	@import "../assets/variable.less";
+	.nav {
+		box-sizing: border-box;
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 50px;
+		background-color: #fff;
+		border-bottom: 1px solid #DFDFDF;
+		z-index: 10;
+	}
+	.hamburger-menu {
+		position: relative;
+		width: 32px;
+		height: 23px;
+		margin-top: 13.5px;
+		.hamburger-item {
+			position: absolute;
+			width: 32px;
+			height: 3px;
+			border-radius: 3px;
+			background-color: @basic-color;
+			-webkit-transform-origin: left center;
+			transform-origin: left center;
+			-webkit-transition: .5s;
+			transition: .5s;
+		}
+		.hamburger-middle {
+			top: 10px;
+			left: 0;
+		}
+		.hamburger-bottom {
+			top: 20px;
+			left: 0;
+		}
+	}
+	.header-search {
+		width: 25px;
+		height: 30px;
+		margin-top: 10px;
+		background: url(../assets/search.png) no-repeat;
+		background-size: contain;
+		cursor: pointer;
+	}
+	.site-name {
+		width: 100%;
+		height: 100%;
+		text-align: center;
+	}
+	.site-logo {
+		display: inline-block;
+		margin-top: 10px;
+		width: 80px;
+	}
+</style>
+
+
---
+++ src/components/app-menu.vue
@@ -0,0 +1,65 @@
+<template>
+	<div class="nav-menu">
+		<div class="container">
+			<ul class="menu-list">
+				<template v-for="menu in menuList">
+					<li class="menu-item" :key="menu.text">
+						<router-link class="menu-link" :to="menu.link" active-class="menu-active" exact>{{ menu.text }}</router-link>
+					</li>
+				</template>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "app-menu",
+	props: {
+		menuList: {
+			required: true,
+			type: Array
+		}
+	}
+}
+</script>
+
+<style lang="less">
+	@import "../assets/variable.less";
+	.nav-menu {
+		position: fixed;
+		top: 50px;
+		width: 100%;
+		height: 50px;
+		background-color: @init-color;
+		z-index: 10;
+		transition: top 0.1s;
+	}
+	.menu-list {
+		width: 100%;
+		height: 50px;
+		white-space: nowrap;
+		overflow: hidden;
+		overflow-x: scroll;
+		-webkit-overflow-scrolling: touch;
+		&::-webkit-scrollbar {
+			display: none;
+		}
+	}
+	.menu-item {
+		display: inline-block;
+		width: auto;
+		font-size: 16px;
+		margin-right: 20px;
+	}
+	.menu-link {
+		line-height: 38px;
+		margin-top: 10px;
+		color: @menu-color;
+	}
+	.menu-active {
+		border-bottom: 2px solid @basic-color;
+	}
+</style>
+
+
