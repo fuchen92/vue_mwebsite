ff30c50cf892adb99ec68aaf2d731f038cfb6462
浮尘 <757213882@qq.com>
2017-09-29 16:13:17 +0800

    修改siderbar组件

--- src/components/siderbar.vue
+++ src/components/siderbar.vue
@@ -3,18 +3,155 @@
 		<div class="container">
 			<div class="close-siderbar">×</div>
 			<div class="category-list-container">
+				<div class="category-list">
+					<h3 class="category-title">新闻</h3>
+					<div class="category-group">
+						<div class="category-item lt">
+							<router-link class="category-link" to="/recommend">
+								<span class="category-icon a1"></span>
+								<span class="category-name">编辑推荐</span>
+							</router-link>
+						</div>
+						<div class="category-item lt">
+							<router-link class="category-link" to="/review">
+								<span class="category-icon c1"></span>
+								<span class="category-name">评论</span>
+							</router-link>
+						</div>
+					</div>
+					<div class="category-group">
+						<div class="category-item lt">
+							<router-link class="category-link" to="/translate">
+								<span class="category-icon b1"></span>
+								<span class="category-name">译讯</span>
+							</router-link>
+						</div>
+						<div class="category-item lt">
+							<router-link class="category-link" to="/lab">
+								<span class="category-icon e1"></span>
+								<span class="category-name">创业</span>
+							</router-link>
+						</div>
+					</div>
+					<div class="category-group">
+						<div class="category-item lt">
+							<router-link class="category-link" to="/digest">
+								<span class="category-icon d1"></span>
+								<span class="category-name">精选</span>
+							</router-link>
+						</div>
+					</div>
+					<div class="divide"></div>
+					<div class="category-group">
+						<div class="category-item lt">
+							<router-link class="category-link" to="/online">
+								<span class="category-icon a2"></span>
+								<span class="category-name">在线旅游</span>
+							</router-link>
+						</div>
+						<div class="category-item lt newline">
+							<router-link class="category-link" to="/hotel">
+								<span class="category-icon b2"></span>
+								<span class="category-name">酒店</span>
+							</router-link>
+						</div>
+					</div>
+					<div class="category-group">
+						<div class="category-item lt">
+							<router-link class="category-link" to="/airline">
+								<span class="category-icon c2"></span>
+								<span class="category-name">航空公司</span>
+							</router-link>
+						</div>
+						<div class="category-item lt newline">
+							<router-link class="category-link" to="/destination">
+								<span class="category-icon d2"></span>
+								<span class="category-name">出入境及目的地</span>
+							</router-link>
+						</div>
+					</div>
+					<div class="category-group">
+						<div class="category-item lt">
+							<router-link class="category-link" to="/investment">
+								<span class="category-icon e2"></span>
+								<span class="category-name">投资并购</span>
+							</router-link>
+						</div>
+						<div class="category-item lt newline">
+							<router-link class="category-link" to="/distributor">
+								<span class="category-icon f2"></span>
+								<span class="category-name">旅游分销商</span>
+							</router-link>
+						</div>
+					</div>
+					<div class="category-group">
+						<div class="category-item lt">
+							<router-link class="category-link" to="/tech">
+								<span class="category-icon g2"></span>
+								<span class="category-name">旅游科技</span>
+							</router-link>
+						</div>
+						<div class="category-item lt newline">
+							<router-link class="category-link" to="/system">
+								<span class="category-icon h2"></span>
+								<span class="category-name">旅游分销系统</span>
+							</router-link>
+						</div>
+					</div>
+					<div class="category-group">
+						<div class="category-item lt">
+							<router-link class="category-link" to="/cruise">
+								<span class="category-icon i2"></span>
+								<span class="category-name">邮轮</span>
+							</router-link>
+						</div>
+						<div class="category-item lt newline">
+							<router-link class="category-link" to="/mice">
+								<span class="category-icon j2"></span>
+								<span class="category-name">商务及奖励旅游</span>
+							</router-link>
+						</div>
+					</div>
+					<div class="category-group">
+						<div class="category-item lt">
+							<router-link class="category-link" to="/carrent">
+								<span class="category-icon k2"></span>
+								<span class="category-name">租车</span>
+							</router-link>
+						</div>
+						<div class="category-item lt newline">
+							<router-link class="category-link" to="/airport">
+								<span class="category-icon l2"></span>
+								<span class="category-name">机场</span>
+							</router-link>
+						</div>
+					</div>
+					<div class="category-group">
+						<div class="category-item lt">
+							<router-link class="category-link" to="/railway">
+								<span class="category-icon m2"></span>
+								<span class="category-name">高铁</span>
+							</router-link>
+						</div>
+						<div class="category-item lt newline">
+							<router-link class="category-link" to="/company">
+								<span class="category-icon n2"></span>
+								<span class="category-name">企业新闻</span>
+							</router-link>
+						</div>
+					</div>
+				</div>
 				<template v-for="(category, index) in categoryList">
 					<div class="category-list" :key="category.name">
 						<h3 class="category-title">{{ category.name }}</h3>
 						<template v-for="(subcategory, idx) in category.subcategory">
 							<div class="category-group" :key="subcategory.text">
-
-									<div class="category-item lt">
-										<router-link class="category-link" :to="subcategory.link">
-											<span class="cagory-icon" :style="{ backgroundImage: subcategory.icon }"></span>
-											<span class="category-name">{{ subcategory.text }}</span>
-										</router-link>
-									</div>
+								<div class="category-item lt only-contain-one">
+									<a class="category-link" :href="subcategory.link">
+										<span class="category-icon" :class="subcategory.className"></span>
+										<span class="category-name">{{ subcategory.text }}</span>
+									</a>
+								</div>
 							</div>
 						</template>
 					</div>
@@ -31,117 +168,19 @@ export default {
 		return {
 			categoryList: [
 				{
-					name: "新闻",
-					subcategory: [
-						{
-							link: "/recommend",
-							icon: 'url(static/img/logo.png)',
-							text: "编辑推荐"
-						},
-						{
-							link: "/review",
-							icon: "./assets/icons/icon-1c.png",
-							text: "评论"
-						},
-						{
-							link: "/translate",
-							icon: "./assets/icons/icon-1b.png",
-							text: "译讯"
-						},
-						{
-							link: "/lab",
-							icon: "./assets/icons/icon-1e.png",
-							text: "创业"
-						},
-						{
-							link: "/digest",
-							icon: "./assets/icons/icon-1d.png",
-							text: "精选"
-						},
-						{
-							link: "/online",
-							icon: "./assets/icons/icon-2a.png",
-							text: "在线旅游"
-						},
-						{
-							link: "/hotel",
-							icon: "./assets/icons/icon-2b.png",
-							text: "酒店"
-						},
-						{
-							link: "/airline",
-							icon: "./assets/icons/icon-2c.png",
-							text: "航空公司"
-						},
-						{
-							link: "/destination",
-							icon: "./assets/icons/icon-2d.png",
-							text: "出入境及目的地"
-						},
-						{
-							link: "/investment",
-							icon: "./assets/icons/icon-2e.png",
-							text: "投资并购"
-						},
-						{
-							link: "/investment",
-							icon: "./assets/icons/icon-2f.png",
-							text: "旅游分销商"
-						},
-						{
-							link: "/tech",
-							icon: "./assets/icons/icon-2g.png",
-							text: "旅游科技"
-						},
-						{
-							link: "/system",
-							icon: "./assets/icons/icon-2h.png",
-							text: "旅游分销系统"
-						},
-						{
-							link: "/cruise",
-							icon: "./assets/icons/icon-2i.png",
-							text: "邮轮"
-						},
-						{
-							link: "/mice",
-							icon: "./assets/icons/icon-2j.png",
-							text: "商务及奖励旅游"
-						},
-						{
-							link: "/carrent",
-							icon: "./assets/icons/icon-2k.png",
-							text: "租车"
-						},
-						{
-							link: "/airport",
-							icon: "./assets/icons/icon-2l.png",
-							text: "机场"
-						},
-						{
-							link: "/railway",
-							icon: "./assets/icons/icon-2m.png",
-							text: "高铁"
-						},
-						{
-							link: "/company",
-							icon: "./assets/icons/icon-2n.png",
-							text: "企业新闻"
-						}
-					]
-				},
-				{
 					name: "活动",
 					subcategory: [
 						{
 							link: "http://event.traveldaily.cn/",
 							icon: "./assets/icons/icon-3a.png",
-							text: "查看全年活动"
+							text: "查看全年活动",
+							className: "a3"
 						},
 						{
 							link: "http://event.traveldaily.cn/packaged/",
 							icon: "./assets/icons/icon-3b.png",
-							text: "购买活动联票"
+							text: "购买活动联票",
+							className: "b3"
 						}
 					]
 				},
@@ -151,12 +190,14 @@ export default {
 						{
 							link: "http://www.traveldaily.cn/marketing/hr",
 							icon: "./assets/icons/icon-4a.png",
-							text: "免费发布招聘信息"
+							text: "免费发布招聘信息",
+							className: "a4"
 						},
 						{
 							link: "/job",
 							icon: "./assets/icons/icon-4b.png",
-							text: "查看最新工作机会"
+							text: "查看最新工作机会",
+							className: "b4"
 						}
 					]
 				},
@@ -166,12 +207,14 @@ export default {
 						{
 							link: "http://www.traveldaily.cn/marketing/service",
 							icon: "./assets/icons/icon-5a.png",
-							text: "精准营销服务"
+							text: "精准营销服务",
+							className: "a5"
 						},
 						{
 							link: "http://www.traveldaily.cn/marketing/workshop",
 							icon: "./assets/icons/icon-5b.png",
-							text: "举办在线讲堂"
+							text: "举办在线讲堂",
+							className: "b5"
 						}
 					]
 				},
@@ -181,12 +224,14 @@ export default {
 						{
 							link: "http://www.chinatravelnews.com/china/?_gscu_=06439806uzkodc66&_gscs_=t06608580pltjny59",
 							icon: "./assets/icons/icon-6a.png",
-							text: "China"
+							text: "China",
+							className: "a6"
 						},
 						{
 							link: "http://www.chinatravelnews.com/international/?_gscu_=06439806uzkodc66&_gscs_=t06608580pltjny59",
 							icon: "./assets/icons/icon-6b.png",
-							text: "International"
+							text: "International",
+							className: "b6"
 						}
 					]
 				}
@@ -238,11 +283,59 @@ export default {
 	.category-group {
 		margin: 0 0 10px;
 	}
+	.divide {
+		width: 150px;
+		height: 1px;
+		background-color: @init-color;
+		margin: 30px 0;
+	}
 	.category-item {
 		width: 50%;
 	}
+	.only-contain-one {
+		width: 100%;
+	}
 	.category-link {
 		color: @init-color;
+	}
+	.category-icon, .category-name {
+		display: inline-block;
+		vertical-align: middle;
+	}
+	.category-icon {
+		width: 25px;
+		height: 25px;
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: contain;
+		margin-right: 5px;
+	}
+	.category-name {
+		font-size: 17px;
+		line-height: 35px;
+	}
+	.a1 {
+		background-image: url(../assets/icons/icon-1a.png);
+	}
+	.c1 {
+		background-image: url(../assets/icons/icon-1c.png);
+	}
+	.a2 {
+		background-image: url(../assets/icons/icon-2a.png);
+	}
+	.b2 {
+		background-image: url(../assets/icons/icon-2b.png);
+	}
+	.generate-background(@n, @i: 1) when (@i <= @n) {
+
+	}
+	@media screen and (max-width: 320px) {
+		.newline {
+			float: none;
+			clear: both;
+			width: 100%;
+			padding-top: 10px;
+		}
 	}
 </style>
 
