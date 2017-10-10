ad16c48c9dfdf922212bfcbfb4c02cecb7e7c530
浮尘 <757213882@qq.com>
2017-10-10 11:48:37 +0800

    更新carousel组件

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -1,5 +1,29 @@
 <template>
-	<div class="carousel swiper-container">
-
+	<div class="carousel carousel-one swiper-container">
+		<ul class="swiper-wrapper">
+			<li class="carousel-item swiper-slide" v-for="carousel in carouselList" :key="carousel.title">
+				<div class="news-img-container">
+					<a :href="carousel.link">
+						<img :src="carousel.img">
+					</a>
+				</div>
+				<p class="news-img-intro">
+					<a :href="carousel.link">{{ carousel.title }}</a>
+				</p>
+				<p class="news-img-intro">{{ carousel.time }}</p>
+			</li>
+		</ul>
 	</div>
 </template>
+
+<script>
+export default {
+	name: "carousel",
+	props: {
+		carouselList: {
+			type: Array
+		}
+	}
+}
+</script>
+
