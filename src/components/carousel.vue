d952a75f4f1f6cff2af35bb5d1b885236459f28e
浮尘 <757213882@qq.com>
2017-10-11 18:20:48 +0800

    添加carousel组件用的图片，修改carousel组件

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -10,7 +10,7 @@
 				<p class="news-img-intro">
 					<a :href="carousel.link">{{ carousel.title }}</a>
 				</p>
-				<p class="news-img-intro">{{ carousel.time }}</p>
+				<p class="news-img-intro" v-if="carousel.time">{{ carousel.time }}</p>
 			</li>
 		</ul>
 	</div>
@@ -27,3 +27,26 @@ export default {
 }
 </script>
 
+<style lang="less">
+@import "../assets/css/swiper-3.4.2.min.css";
+.carousel {
+	width: 100%;
+	padding-bottom: 20px;
+	border-bottom: 1px solid #e4e4e4;
+}
+.carousel-item {
+	width: 211px;
+	margin-right: 15px;
+}
+.news-img-container a, .news-img-container img {
+	width: 100%;
+}
+.news-img-intro {
+	margin-top: 15px;
+	font-size: 16px;
+	> a {
+		color: #333333;
+	}
+}
+
+</style>
--- src/views/list-default.vue
+++ src/views/list-default.vue
@@ -4,6 +4,7 @@
 		<div class="container">
 			<banner></banner>
 			<news-category :newsCategory="newsCategory"></news-category>
+			<carousel-one :carouselList="carouselList"></carousel-one>
 		</div>
 	</div>
 </template>
@@ -12,6 +13,7 @@
 import banner from "@/components/banner.vue";
 // import slider from "@/components/slider.vue";
 import newsCategory from "@/components/news-category.vue";
+import carouselOne from "@/components/carousel.vue";
 export default {
 	name: "list-default",
 	data () {
@@ -20,13 +22,40 @@ export default {
 				link: "",
 				type: "编辑推荐",
 				moretext: "更多>>"
-			}
+			},
+			carouselList: [
+				{
+					img: require("../assets/carousel-1.jpg"),
+					link: "",
+					title: "“你收到一条好友请求”",
+					time: "2017-10-11"
+				},
+				{
+					img: require("../assets/carousel-2.jpg"),
+					link: "",
+					title: "增值业务，是金牛还是鸡肋？",
+					time: "2017-10-11"
+				},
+				{
+					img: require("../assets/carousel-3.jpg"),
+					link: "",
+					title: "伊斯坦布尔新机场将部署SITA技术实现100%行李追踪",
+					time: "2017-10-10"
+				},
+				{
+					img: require("../assets/carousel-2.jpg"),
+					link: "",
+					title: "首个民宿国家行业标准生效　但短租仍处于灰色地带",
+					time: "2017-10-10"
+				}
+			]
 		}
 	},
 	components: {
 		// "slider": slider
 		"banner": banner,
-		"news-category": newsCategory
+		"news-category": newsCategory,
+		"carousel-one": carouselOne
 	}
 }
 </script>
