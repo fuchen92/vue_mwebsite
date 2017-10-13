3b0eb932fcc6768190d4bf20632870103e0e7e69
浮尘 <757213882@qq.com>
2017-10-13 14:25:16 +0800

    修改目录结构，把mock数据都放到同一个文件夹下方便管理

--- mock/router-news.js
+++ mock/router-news.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var newsList = require("../newslist.json");
+var newsList = require("./newslist.json");
 //对所有新闻的get进行mock
 router.get('/news', function (req, res, next) {
 	//响应mock数据
